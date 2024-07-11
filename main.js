0b96278b9fd4531310257f9102c3c57a05886a61
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-07-11 19:44:03 -0400

    add: share icons and dropdown menu

---
+++ main.js
@@ -0,0 +1,20 @@
+function dropdownOnClick(dropdownContainer, dropdownMenu, className = 'show') {
+  dropdownContainer.addEventListener('click', () => {
+    dropdownMenu.classList.toggle(className)
+  })
+
+  dropdownContainer.addEventListener('mouseleave', () => {
+    dropdownMenu.classList.remove(className)
+  })
+}
+
+const dropdownContainers = document.querySelectorAll('.dropdown')
+dropdownContainers.forEach((e) => {
+  const dropdownChildren = [...e.children]
+
+  for (let child of dropdownChildren) {
+    if (child.classList.contains('dropdown-content')) {
+      dropdownOnClick(e, child)
+    }
+  }
+})
