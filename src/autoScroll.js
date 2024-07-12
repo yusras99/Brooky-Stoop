5139ca77aa12d6948fe0b06c659bd08eb11fcd3f
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-07-12 04:40:34 -0400

    feature: auto scroll carousel

---
+++ src/autoScroll.js
@@ -0,0 +1,41 @@
+const scrollContainers = document.querySelectorAll("#infiniteScroll--left");
+
+scrollContainers.forEach((container) => {
+	const scrollWidth = container.scrollWidth;
+	let isScrollingPaused = false;
+
+	window.addEventListener("load", () => {
+		self.setInterval(() => {
+			if (isScrollingPaused) {
+				return;
+			}
+			const first = container.querySelector("article");
+
+			if (!isElementInViewport(first)) {
+				container.appendChild(first);
+				container.scrollTo(container.scrollLeft - first.offsetWidth, 0);
+			}
+			if (container.scrollLeft !== scrollWidth) {
+				container.scrollTo(container.scrollLeft + 1, 0);
+			}
+		}, 15);
+	});
+
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return rect.right > 0;
+	}
+
+	function pauseScrolling() {
+		isScrollingPaused = true;
+	}
+
+	function resumeScrolling() {
+		isScrollingPaused = false;
+	}
+	const allArticles = container.querySelectorAll("article");
+	for (let article of allArticles) {
+		article.addEventListener("mouseenter", pauseScrolling);
+		article.addEventListener("mouseleave", resumeScrolling);
+	}
+});
