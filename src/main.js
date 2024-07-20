2c6c8496d8862b006a518d62a30d3ea2190d8ecf
yusra_suhail@uri.edu <Yusra Suhail>
2024-07-20 19:31:17 -0400

    changed rotation and imgs for objects

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import '../styles/style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import ballImg from '/assets/ball.jpg';
+import basketBallImg from '/assets/basketball.jpeg';
 import rubikImg from '/assets/rubik.jpg';
 import shirtImg from '/assets/shirt.png';
+import shirt2Img from '/assets/shirt2.png';
 import brownStoneTexture from '/assets/bg_cropped.jpg';
 import starImg from '/assets/star.png';
 
@@ -18,43 +20,47 @@ function addStar() {
 
   const star = new THREE.Mesh(starGeometry, starMaterial);
   star.userData.type = 'star';
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-
+  
+  const x = THREE.MathUtils.randFloat(-10, 5) - 20; // Negative x for left side
+  const y = THREE.MathUtils.randFloat(-15, -5); 
+  const z = 5;
+  
   star.position.set(x, y, z);
   scene.add(star);
 }
 
-function addShirt() {
+function addShirt(img) {
   // Add shirt
   const shirtGeometry = new THREE.BoxGeometry(3, 3, 0.1);
   const shirtMaterial = new THREE.MeshPhongMaterial({
-    map: new THREE.TextureLoader().load(shirtImg),
+    map: new THREE.TextureLoader().load(img),
   });
 
   shirt = new THREE.Mesh(shirtGeometry, shirtMaterial);
   shirt.userData.type = 'shirt';
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+  const x = THREE.MathUtils.randFloat(2, 6) - 20; // Negative x for left side
+  const y = THREE.MathUtils.randFloat(-15, -2); 
+  const z = 5;
+
   shirt.position.set(x, y, z);
   scene.add(shirt);
 }
 
-function addBall() {
+function addBall(img) {
   // Add ball
   const ballGeometry = new THREE.SphereGeometry(1, 32, 32);
   const ballMaterial = new THREE.MeshPhongMaterial({
-    map: new THREE.TextureLoader().load(ballImg),
+    map: new THREE.TextureLoader().load(img),
+    // emissive: new THREE.Color(0x333333), // Glow effect
+    // shininess: 100
   });
   ball = new THREE.Mesh(ballGeometry, ballMaterial);
 
   ball.userData.type = 'ball';
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+  const x = THREE.MathUtils.randFloat(-6, 1) - 20; // Negative x for left side
+  const y = THREE.MathUtils.randFloat(-15, -2); 
+  const z = 5;
   ball.position.set(x, y, z);
   scene.add(ball);
 }
@@ -67,17 +73,13 @@ function addBox() {
 
   box = new THREE.Mesh(boxGeometry, material);
   box.userData.type = 'box';
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+  box.renderOrder = 1;
+  const x = THREE.MathUtils.randFloat(-12, -8) - 20; // Negative x for left side
+  const y = THREE.MathUtils.randFloat(-15, -2); 
+  const z = 5;
+
   box.position.set(x, y, z);
   scene.add(box);
-
-  // Add edges on the box
-  const edgesGeometry = new THREE.EdgesGeometry(boxGeometry);
-  const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Black color for edges
-  const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
-  box.add(edges);
 }
 
 function init() {
@@ -91,7 +93,7 @@ function init() {
     0.1,
     1000
   );
-  camera.position.z = 5;
+  camera.position.z = 30;
 
   // Renderer
   const bg_container = document.querySelector('.bg');
@@ -102,12 +104,15 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   bg_container.appendChild(renderer.domElement);
   
-  bg_geometry = new THREE.PlaneGeometry(14, 8, 15, 9);
+  bg_geometry = new THREE.PlaneGeometry(80, 50, 15, 9);
   const bg_material = new THREE.MeshBasicMaterial({
     map : loader.load(brownStoneTexture),
+    transparent : true,
+    opacity: 1
   });
   const bg_mesh = new THREE.Mesh(bg_geometry,bg_material);
-  scene.add(bg_mesh);
+  bg_mesh.position.z = 1; // Ensure background is behind other objects
+  bg_mesh.renderOrder = 0;
 
 
   // Add lights
@@ -118,11 +123,13 @@ function init() {
   directionalLight.position.set(1, 1, 1).normalize();
   scene.add(directionalLight);
 
-  // Array(100).fill().forEach(addStar);
-  // Array(100).fill().forEach(addShirt);
-  // Array(100).fill().forEach(addBall);
-  // Array(100).fill().forEach(addBox);
+  Array(3).fill().forEach(() => addShirt(shirtImg));
+  Array(3).fill().forEach(() => addShirt(shirt2Img));
+  Array(3).fill().forEach(() => addBall(ballImg));
+  Array(3).fill().forEach(() => addBall(basketBallImg));
+  Array(5).fill().forEach(addBox);
 
+  scene.add(bg_mesh);
   // update camera position by listening to dom events by mouse
   controls = new OrbitControls(camera, renderer.domElement);
 
@@ -153,23 +160,20 @@ function animate() {
     bg_geometry.computeVertexNormals();
     // position attribute of the geometry has been updated and needs to be reprocessed for rendering
     bg_geometry.attributes.position.needsUpdate = true;
+
+    scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        if(child.userData.type == 'ball' || child.userData.type ==  'shirt' || child.userData.type == 'box') {
+            child.rotation.x += 0.0001; // Slow down rotation
+            child.position.y += Math.sin(time * 1.5) * 0.001; // Slow down up and down movement
+        }
+      }
+    });
   }
 
 
 
   requestAnimationFrame(animate); // Request next animation frame
-
-  // Rotate objects if they exist
-  scene.traverse((child) => {
-    if (
-      (child instanceof THREE.Mesh && child.userData.type === 'shirt') ||
-      (child instanceof THREE.Mesh && child.userData.type === 'star') ||
-      (child instanceof THREE.Mesh && child.userData.type === 'box') ||
-      (child instanceof THREE.Mesh && child.userData.type === 'ball')
-    ) {
-      child.rotation.y += 0.01; // Rotate shirt around Y axis
-    }
-  });
 
   controls.update(); // Update orbit controls
 
